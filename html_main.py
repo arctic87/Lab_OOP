e317fba6c841e437db53701aa67564ef456e89d4
Чижевский <50781178+arctic87@users.noreply.github.com>
2020-05-30 14:30:19 +0300

    HTML-парсер (Главный модуль)

--- html_main.py
+++ html_main.py
@@ -1,12 +1,15 @@
 from html_parser import Parser
 
+#собираем данные с сайта
 list1 = Parser.url_reguest(None, "http://google.com")
 list2 = Parser.url_reguest(None, "http://google.com")
 
+#ищем разницу между списками
 search_different = list(set(list1)-set(list2))
 
 print(search_different)
 
 print(list1[5])
+#выводим данные со второго уровня сайтов
 list_level2 = Parser.url_reguest(None, list1[5])
 print(list_level2)
