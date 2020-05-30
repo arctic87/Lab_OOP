7bcf93cb0c2e1ccc25f0e6f0e67a17ad19c358ba
Чижевский <50781178+arctic87@users.noreply.github.com>
2020-05-30 14:32:41 +0300

    HTML-парсер (класс Parser)

--- html_parser.py
+++ html_parser.py
@@ -5,10 +5,12 @@ import re
 class Parser:
     def url_reguest(self, request):
         url = requests.get(request)
-
+        
+        #используем фреймворк
         soup = BeautifulSoup(url.text, "html.parser")
         links = []
-
+        
+        #ищем ссылки с заданными параметрами
         for link in soup.findAll('a'):
             link = link.get('href')
             if link.startswith('http'):
