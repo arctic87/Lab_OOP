1241cd2f3fc29f2ba71c7f2983b1e3ff4bc4907e
Чижевский <50781178+arctic87@users.noreply.github.com>
2020-05-31 21:57:08 +0300

    Главный модуль (выводит расстояние и площадь)

--- Point_main.py
+++ Point_main.py
@@ -1,16 +1,22 @@
 import math
-from Point import Point
 from Point_3d import Point3d
 from Point_triangle import Triangle
 
-#Создаем 2 точки и определяем расстояние между ними
-p1 = Point3d(10, 12, 24)
-p2 = Point3d(15, 25, 50)
-p1.showInfo()
-p2.showInfo()
-print("Расстояние между точками:", p1.distance(p2.getXYZ()))
+# Создаем 3 точки c координатами x,y,z
+p1 = Point3d(1, 2, 4)
+p2 = Point3d(5, 10, 15)
+p3 = Point3d(3, 6, 9)
 
-#Создаем треугольник и определяем его площадь
-t1 = Triangle(10, 12, 24, 15, 25, 50)
-t1.showInfo()
-print("Площадь заданного треугольника:", t1.area())
+# Вычисляем расстояния между этими точками
+a = p1.distanceTo(p2)
+b = p2.distanceTo(p3)
+c = p3.distanceTo(p1)
+
+# Выводим расстояния с 2 знаками после запятой
+print("Расстояние a:", "%.2f" % a)
+print("Расстояние b:", "%.2f" % b)
+print("Расстояние c:", "%.2f" % c)
+
+# Выводим расстояния с 2 знаками после запятой
+area = Triangle.computeArea(a, b, c)
+print("Площадь треугольника = " "%.2f" % area)
