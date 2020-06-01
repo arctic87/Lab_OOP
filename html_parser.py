43637987fe7195e8ff96c66edc1268c5ac3f7a0b
Чижевский <50781178+arctic87@users.noreply.github.com>
2020-06-01 16:43:03 +0300

    HTML-Parser (Класс Parser - выборка ссылок)

--- html_parser.py
+++ html_parser.py
@@ -3,24 +3,24 @@ from urllib.request import urlopen
 import re
 
 
-
 class Parser:
-    # получение начальной страницы
     def url_request(self, url):
+        # получение начальной страницы
         page = urlopen(url)
-
         soup = BeautifulSoup(page.read(), "html.parser")
         # Задаем список ссылок
         links = []
 
         # Выборка ссылок со страницы
-        for link in soup.findAll('a'):
+        for link in soup.find_all('a'):
             link = link.get('href')
-            if link.startswith('http'):
-                links.append(link)
+            if link.startswith("http"):
+                links.append([link])
             else:
                 break
         return links
+
+
 
 
 
